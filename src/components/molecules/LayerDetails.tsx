ef85709be7c4d9a81ee4ef933f09149b98fb3b56
imloreno <work@imloreno.com>
2024-10-26 07:33:46 -0400

    Adding no-image in details section

--- src/components/molecules/LayerDetails.tsx
+++ src/components/molecules/LayerDetails.tsx
@@ -3,8 +3,12 @@ import { Subtitle } from "@atoms";
 const LayerDetails = () => {
   return (
     <section>
-      <div className="aspect-video bg-[#1D1D26]">
-        <img src="" alt="" />
+      <div className="aspect-video bg-[#1D1D26] overflow-hidden">
+        <img
+          src="no_image.png"
+          alt="No imge"
+          className="w-full h-full object-cover"
+        />
       </div>
       <Subtitle className="px-6 py-4">Detalles</Subtitle>
       <div className=""></div>
